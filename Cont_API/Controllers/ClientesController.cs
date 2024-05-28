76741bdb1dc0969c021bfcc6f929b37605c9307b
Ricardo Nieves <ricardo.nieves@outlook.com>
2024-05-27 21:42:49 -0500

    Archivos del proyecto de Cont_API

---
+++ Cont_API/Controllers/ClientesController.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+[ApiController]
+[Route("[controller]")]
+public class ClientesController : ControllerBase
+{
+    private readonly MyDbContext _context;
+
+    public ClientesController(MyDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Cliente>>> GetClientes()
+    {
+        return await _context.Clientes.ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Cliente>> GetCliente(int id)
+    {
+        var cliente = await _context.Clientes.FindAsync(id);
+
+        if (cliente == null)
+        {
+            return NotFound();
+        }
+
+        return cliente;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Cliente>> PostCliente(Cliente cliente)
+    {
+        _context.Clientes.Add(cliente);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction(nameof(GetCliente), new { id = cliente.IdCliente }, cliente);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutCliente(int id, Cliente cliente)
+    {
+        if (id != cliente.IdCliente)
+        {
+            return BadRequest();
+        }
+
+        _context.Entry(cliente).State = EntityState.Modified;
+
+        try
+        {
+            await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!ClienteExists(id))
+            {
+                return NotFound();
+            }
+            else
+            {
+                throw;
+            }
+        }
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteCliente(int id)
+    {
+        var cliente = await _context.Clientes.FindAsync(id);
+        if (cliente == null)
+        {
+            return NotFound();
+        }
+
+        _context.Clientes.Remove(cliente);
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+    }
+
+    private bool ClienteExists(int id)
+    {
+        return _context.Clientes.Any(e => e.IdCliente == id);
+    }
+}
